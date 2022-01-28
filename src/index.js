cec3f52a836e8d3c89c23b548d957d7f2e2a2f71
JonatSrG <JonatSrG>
2022-01-27 20:13:34 -0600

    creacion de los nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,32 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo"
+
+//web api
+//Conectarnos al servidor
+window.fetch(url)
+//procesar la respuesta y convertirla en JSON
+.then((respuesta) => respuesta.json())
+//JSON -> Renderizar info browser
+.then((responseJson) => {
+    const todosLosItems = [];
+    responseJson.data.forEach((item) => {
+        //crear imegen
+        const imagen = document.createElement('img');
+
+        //crear titulo
+        const title = document.createElement("h2");
+
+        //crear precio
+        const price = document.createElement("div");
+
+        const container = document.createElement("div");
+        container.append(imagen, title, price);
+
+        todosLosItems.push(container);
+    });
+
+    document.body.append(...todosLosItems);
+})
+
